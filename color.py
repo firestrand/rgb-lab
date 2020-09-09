7ea18a2a5610286bf9b17fc0202fe58aeefafde1
Travis Silvers <firestrand@gmail.com>
2020-09-09 11:23:46 -0700

    Clean up after move

--- color.py
+++ color.py
@@ -1,19 +1,25 @@
 import math
+
 '''
 References:
 http://brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
 https://en.wikipedia.org/wiki/Color_difference
 '''
-#Constants
+# Constants
 # CIE Standard ϵ = 0.008856 ~ Intent 216/24389
-epsilon = 216./24389.
+epsilon = 216. / 24389.
 # CIE Standard κ = 903.3 ~ Intent 24389/27
-kappa = 24389./27.
+kappa = 24389. / 27.
+
 
 def compand(t):
     return (1.055 * math.pow(t, 1. / 2.4) - 0.055) if (t > 0.0031308) else 12.92 * t
+
+
 def r_f(t):
-    return math.pow(t, 3) if (math.pow(t, 3) > epsilon) else (t - 16./116.) / 7.787
+    return math.pow(t, 3) if (math.pow(t, 3) > epsilon) else (t - 16. / 116.) / 7.787
+
+
 def lab2rgb(lab):
     y = (lab[0] + 16.) / 116.
     x = lab[1] / 500. + y
@@ -23,9 +29,9 @@ def lab2rgb(lab):
     y = 1.000000 * r_f(y)
     z = 1.088754 * r_f(z)
 
-    r = x *  3.2404542 + y * -1.5371385 + z * -0.4985314
-    g = x * -0.9692660 + y *  1.8760108 + z *  0.0415560
-    b = x *  0.0556434 + y * -0.2040259 + z *  1.0572252
+    r = x * 3.2404542 + y * -1.5371385 + z * -0.4985314
+    g = x * -0.9692660 + y * 1.8760108 + z * 0.0415560
+    b = x * 0.0556434 + y * -0.2040259 + z * 1.0572252
 
     r = compand(r)
     g = compand(g)
@@ -35,24 +41,28 @@ def lab2rgb(lab):
             max(0, min(1, g)) * 255,
             max(0, min(1, b)) * 255]
 
+
 def inverse_compand(t):
     return math.pow((t + 0.055) / 1.055, 2.4) if (t > 0.04045) else t / 12.92
 
+
 def f(t):
     # CIE Standard 0.008856 ~ Intent 216/24389
-    return math.pow(t, 1/3) if t > epsilon else kappa/116. + t + 16./116.
+    return math.pow(t, 1 / 3) if t > epsilon else kappa / 116. + t + 16. / 116.
+
+
 def rgb2lab(rgb):
     """
     based on https://docs.opencv.org/3.1.0/de/d25/imgproc_color_conversions.html#color_convert_rgb_lab
     :param rgb:
     :return:
     """
-    #Scale from 0-1
+    # Scale from 0-1
     r = rgb[0] / 255.
     g = rgb[1] / 255.
     b = rgb[2] / 255.
 
-    #made linear with respect to energy
+    # made linear with respect to energy
     r = inverse_compand(r)
     g = inverse_compand(g)
     b = inverse_compand(b)
@@ -66,11 +76,12 @@ def rgb2lab(rgb):
     fy = f(y)
     fz = f(z)
 
-    L = 116. * pow(y, 1./3.) - 16. if y > epsilon else 903.3 * y
+    L = 116. * pow(y, 1. / 3.) - 16. if y > epsilon else 903.3 * y
     a = 500. * (fx - fy)
     b = 200. * (fy - fz)
 
     return [L, a, b]
+
 
 def deltaE(labA, labB):
     """
@@ -97,4 +108,4 @@ def deltaE(labA, labB):
     return math.sqrt(i)
 
 
-print(lab2rgb(rgb2lab([255,128,0])))
+print(lab2rgb(rgb2lab([255, 128, 0])))
